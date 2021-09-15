49e70b871fa015d3a9ae2b60eba9784d2bb7d0dd
Opilane TTHK <opilane@tthk.ee>
2021-09-15 09:41:16 +0300

    RandomNumberGenerators

---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int myRandomNumber = rnd.Next(1, 11);// number 1 kuni 10
+
+            Console.WriteLine($"Sinu juhuslik number on {myRandomNumber}");
+
+            //programm kontrollib, kas genereeritud number on suurem kui 5
+            //kui genereeritud number on suurem kui 5 programm kuvab
+            //""juhuslik number on {myRandomNumber}, see on suurem kui 5"
+            //kui genereeritud number on väiksem, kui 5, siis programm kuvab
+            //""juhuslik number on  {myRandomNumber}, see on väiksem kui 5"
+            //kui genereeritud number on 5, programm kuvab
+            //"juhuslik number on 5"
+
+            if (myRandomNumber > 5)
+            {
+                Console.WriteLine($"Juhuslik number on {myRandomNumber}, see on suurem kui 5");
+            }
+            else if (myRandomNumber < 5)
+            {
+                Console.WriteLine($"Juhuslik number on {myRandomNumber}, see on väiksem kui 5");
+            }
+            else
+            {
+                Console.WriteLine("Juhuslik number on 5");
+            }
+        }
+    }
+}
